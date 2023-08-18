fc5d5a29a965ce1c2017a9bc177b16b51d751462
changhee1234 <126552104+changhee1234@users.noreply.github.com>
2023-08-18 18:51:38 +0900

    Delete camp directory

--- camp/src/page/layout/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'http://localhost:8080', // 스프링 서버 URL
-    withCredentials: true, // CORS 관련 설정
-});
-
-export default instance;
