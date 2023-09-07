c5da1e4be40e77041abd00c53c572ce855489e58
seinJeong5 <jhkk345@naver.com>
2023-09-07 09:55:11 +0900

    no message

--- camp/src/main/java/com/bitc/camp/data/dto/CampImgDto.java
+++ camp/src/main/java/com/bitc/camp/data/dto/CampImgDto.java
@@ -1,19 +1,19 @@
-package com.bitc.camp.data.dto;
-
-import com.bitc.camp.data.entity.CampMainInfo;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class CampImgDto {
-    private int idx;
-    private String originalFileName;
-    private String storedFileName;
-    private int fileSize;
-    private CampMainInfo campMainInfo;
-}
+//package com.bitc.camp.data.dto;
+//
+//import com.bitc.camp.data.entity.CampMainInfo;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class CampImgDto {
+//    private int idx;
+//    private String originalFileName;
+//    private String storedFileName;
+//    private int fileSize;
+//    private CampMainInfo campMainInfo;
+//}
--- camp/src/main/java/com/bitc/camp/data/repository/CampImgRepository.java
+++ camp/src/main/java/com/bitc/camp/data/repository/CampImgRepository.java
@@ -1,7 +1,7 @@
-package com.bitc.camp.data.repository;
-
-import com.bitc.camp.data.entity.CampImg;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampImgRepository extends JpaRepository<CampImg, Integer> {
-}
+//package com.bitc.camp.data.repository;
+//
+//import com.bitc.camp.data.entity.CampImg;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface CampImgRepository extends JpaRepository<CampImg, Integer> {
+//}
